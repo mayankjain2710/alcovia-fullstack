b5923fde0ba001b80fa94b8bf5938d7fb0d495c4
Mayank Jain <mayankmaster2004@gmail.com>
2025-11-25 13:31:44 +0530

    updates

--- alcovia-backend/server.js
+++ alcovia-backend/server.js
@@ -4,7 +4,9 @@ import studentRoutes from "./routes/studentRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use("/api", studentRoutes);
