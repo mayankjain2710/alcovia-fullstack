8cc14986cdfdbf714d8d73147cfb2f88fd132fcd
Mayank Jain <mayankmaster2004@gmail.com>
2025-11-25 13:37:51 +0530

    updated files

--- alcovia-backend/server.js
+++ alcovia-backend/server.js
@@ -7,10 +7,13 @@ const app = express();
 app.use(cors({
   origin: '*'
 }));
+
 app.use(express.json());
 
 app.use("/api", studentRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
